f7337d5564414ce969c04f64b278f9289c24dd16
wuppo <wobjelsm@gmail.com>
2024-05-27 11:44:54 +0200

    fix: removed old code

--- applicatie/routes/page-routes.php
+++ applicatie/routes/page-routes.php
@@ -2,5 +2,4 @@
 
 \Controller\RouteController::addRoute('/', 'views/templates/home.php', 'Home');
 \Controller\RouteController::addRoute('/404', 'views/templates/404.php', '404');
-
-\Controller\RouteController::addRoute('/sheet', 'views/templates/character-sheet.php', 'CharacterSheet');
+\Controller\RouteController::addRoute('/info', 'info.php', 'info');
