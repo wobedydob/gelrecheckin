faf5757fcac494dc891b6c203df87c6eb499665f
wuppo <wobjelsm@gmail.com>
2024-06-07 17:15:39 +0200

    styling

--- app/views/templates/home.php
+++ app/views/templates/home.php
@@ -10,6 +10,11 @@
         <h1>Welkom <?php echo auth()->user()->getModel()?->naam; ?></h1>
     <?php endif; ?>
 
+    <?php if (auth()->withRole('service_desk')): ?>
+        <?php $deskId = auth()->user()->getModel()?->balienummer; ?>
+        <h1><?php echo $deskId ? 'Balienummer ' . $deskId : 'Onbekend balienummer'; ?></h1>
+    <?php endif; ?>
+
     <p>This is basic content...</p>
 
 </main>
