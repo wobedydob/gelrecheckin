38169a7f259f554d4bc90e4b389ace5bf85ae569
wuppo <wobjelsm@gmail.com>
2024-06-07 14:16:14 +0200

    feat: added styling

---
+++ app/assets/js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navbarToggle = document.getElementById('navbar-toggle');
+    const navbarMenu = document.getElementById('navbar-menu');
+
+    navbarToggle.addEventListener('click', () => {
+        navbarMenu.classList.toggle('active');
+    });
+});
