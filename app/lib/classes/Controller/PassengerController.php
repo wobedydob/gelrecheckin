cb2ccc8ce708322cecf2acf446ba82242866d976
wuppo <wobjelsm@gmail.com>
2024-06-09 19:50:01 +0200

    fix: password hashing and unhashing capabilities

--- app/lib/classes/Controller/PassengerController.php
+++ app/lib/classes/Controller/PassengerController.php
@@ -2,6 +2,7 @@
 
 namespace Controller;
 
+use Model\Flight;
 use Model\Passenger;
 use Service\Redirect;
 use Service\Session;
@@ -28,25 +29,25 @@ class PassengerController
         $passengerId = $post['passenger_id'] ? StringHelper::sanitize($post['passenger_id']) : false;
         $password = $post['password'] ?: false;
 
+        $serviceDesk = Passenger::with(['wachtwoord'])->where('passagiernummer', '=', $passengerId)->first();
+        $verify = password_verify($password, $serviceDesk->wachtwoord);
+        $secondVerify = Passenger::where('passagiernummer', '=', $passengerId)->where('wachtwoord', '=', $password)->exists(); // only because the given database has not used any hashed passwords... stupid han sometimes...
+
         if (!$passengerId) {
             $this->errors['passenger_id'] = 'none given';
         }
 
-        if (!$password) {
-            $this->errors['password'] = 'none given';
-        }
-
-        if (!Passenger::where('passagiernummer', '=', $passengerId)->where('wachtwoord', '=', $password)->exists()) {
+        if (!$verify && !$secondVerify) {
             $this->errors['error'] = 'invalid credentials';
         }
 
         if(!empty($this->errors)) {
+            dd($this->errors);
             View::new()->render('views/templates/passenger/passenger-login.php', ['errors' => $this->errors]);
             return;
         }
 
         $user = ['id' => (int) $passengerId, 'role' => 'passenger'];
-
         Session::instance()->set('user', $user);
         Redirect::to('/dashboard');
     }
@@ -64,12 +65,23 @@ class PassengerController
         View::new()->render('views/templates/service-desk/service-desk-passengers.php');
     }
 
+    public function show($id): void
+    {
+        $passenger = Passenger::find($id);
+
+        if (!$passenger) {
+            Redirect::to('/passagiers');
+        }
+
+        View::new()->render('views/templates/passenger.php', compact('passenger'));
+    }
+
     public function add(): void
     {
         View::new()->render('views/templates/passenger-add.php');
     }
 
-    public function addFlight(): void
+    public function addPassenger(): void
     {
         $post = $_POST;
 
@@ -78,7 +90,7 @@ class PassengerController
         }
 
         $name = $post['name'] ? StringHelper::sanitize($post['name']) : false;
-        $flightId = $post['flight_id'] ? StringHelper::sanitize($post['flight_id']) : false;
+        $passengerId = $post['passenger_id'] ? StringHelper::sanitize($post['passenger_id']) : false;
         $gender = $post['gender'] ? StringHelper::sanitize($post['gender']) : false;
         $deskId = $post['desk_id'] ? StringHelper::sanitize($post['desk_id']) : false;
         $seat = $post['seat'] ? StringHelper::sanitize($post['seat']) : false;
@@ -90,8 +102,8 @@ class PassengerController
             $this->errors['name'] = 'none given';
         }
 
-        if (!$flightId) {
-            $this->errors['flight_id'] = 'none given';
+        if (!$passengerId) {
+            $this->errors['passenger_id'] = 'none given';
         }
 
         if (!$gender) {
@@ -115,26 +127,111 @@ class PassengerController
         }
 
         if (!empty($this->errors)) {
-            View::new()->render('views/templates/flight-add.php', ['errors' => $this->errors]);
+            View::new()->render('views/templates/passenger-add.php', ['errors' => $this->errors]);
             return;
         }
 
         $action = Passenger::create([
             'naam' => $name,
-            'vluchtnummer' => $flightId,
+            'vluchtnummer' => $passengerId,
             'geslacht' => $gender,
             'balienummer' => $deskId,
             'stoel' => $seat,
             'inchecktijdstip' => $checkinTime,
             'wachtwoord' => $password,
         ]);
-//
-//        if(!$action) {
-//            $error = ['errors' => 'Flight could not be added'];
-//            View::new()->render('views/templates/flight-add.php', $error);
-//        }
-//
-//        Redirect::to('/vluchten');
+
+        if(!$action) {
+            $error = ['errors' => 'Passenger could not be added'];
+            View::new()->render('views/templates/passenger-add.php', $error);
+        }
+
+        Redirect::to('/vluchten');
     }
 
+    public function edit($id): void
+    {
+        $passenger = Passenger::find($id);
+
+        if (!$passenger) {
+            Redirect::to('/vluchten');
+        }
+
+        View::new()->render('views/templates/passenger-edit.php', compact('passenger'));
+    }
+
+    public function editPassenger($id): void
+    {
+        $post = $_POST;
+
+        if (!isset($post['submit'])) {
+            return;
+        }
+
+        $destination = $post['destination'] ? StringHelper::sanitize($post['destination']) : false;
+        $gate = $post['gate_id'] ? StringHelper::sanitize($post['gate_id']) : false;
+        $maxLimit = $post['max_limit'] ? StringHelper::sanitize($post['max_limit']) : false;
+        $maxWeightPP = $post['max_weight_pp'] ? StringHelper::sanitize($post['max_weight_pp']) : false;
+        $maxTotalWeight = $post['max_total_weight'] ? StringHelper::sanitize($post['max_total_weight']) : false;
+        $departure = $post['departure_time'] ? StringHelper::toDateTime($post['departure_time']) : false;
+        $airline = $post['airline_id'] ? StringHelper::sanitize($post['airline_id']) : false;
+
+        if (!$destination) {
+            $this->errors['destination'] = 'none given';
+        }
+
+        if (!$gate) {
+            $this->errors['gate_id'] = 'none given';
+        }
+
+        if (!$maxLimit) {
+            $this->errors['max_limit'] = 'none given';
+        }
+
+        if (!$maxWeightPP) {
+            $this->errors['max_weight_pp'] = 'none given';
+        }
+
+        if (!$maxTotalWeight) {
+            $this->errors['max_total_weight'] = 'none given';
+        }
+
+        if (!$departure) {
+            $this->errors['departure_time'] = 'none given';
+        }
+
+        if (!$airline) {
+            $this->errors['airline_id'] = 'none given';
+        }
+
+        if (!empty($this->errors)) {
+            View::new()->render('views/forms/passenger-edit-form.php', ['errors' => $this->errors]);
+            return;
+        }
+
+        $action = \Model\Passenger::where('vluchtnummer', '=', $id)->update([
+            'vluchtnummer' => $id,
+            'bestemming' => $destination,
+            'gatecode' => $gate,
+            'max_aantal' => $maxLimit,
+            'max_gewicht_pp' => $maxWeightPP,
+            'max_totaalgewicht' => $maxTotalWeight,
+            'vertrektijd' => $departure,
+            'maatschappijcode' => $airline,
+        ]);
+
+        if(!$action) {
+            $error = ['errors' => 'Passenger could not be edited'];
+            View::new()->render('views/templates/passenger-add.php', $error);
+        }
+
+        Redirect::to('/vluchten');
+    }
+
+    public function delete($id): void
+    {
+        Passenger::where('vluchtnummer', '=', $id)->delete();
+        Redirect::to('/vluchten');
+    }
+    
 }
--- app/lib/classes/Controller/ServiceDeskController.php
+++ app/lib/classes/Controller/ServiceDeskController.php
@@ -28,6 +28,14 @@ class ServiceDeskController
         $deskId = $post['desk_id'] ? \Util\StringHelper::sanitize($post['desk_id']) : false;
         $password = $post['password'] ?: false;
 
+        $serviceDesk = ServiceDesk::with(['wachtwoord'])->where('balienummer', '=', $deskId)->first();
+        $verify = password_verify($password, $serviceDesk->wachtwoord);
+        $secondVerify = ServiceDesk::where('balienummer', '=', $deskId)->where('wachtwoord', '=', $password)->exists(); // only because the given database has not used any hashed passwords... stupid han sometimes...
+
+        if (!$verify && !$secondVerify) {
+            $this->errors['error'] = 'invalid credentials';
+        }
+
         if (!$deskId) {
             $this->errors['desk_id'] = 'none given';
         }
@@ -36,16 +44,12 @@ class ServiceDeskController
             $this->errors['password'] = 'none given';
         }
 
-        if (!ServiceDesk::where('balienummer', '=', $deskId)->where('wachtwoord', '=', $password)->exists()) {
-            $this->errors['error'] = 'invalid credentials';
-        }
-
-        if(!empty($this->errors)) {
+        if (!empty($this->errors)) {
             View::new()->render('views/templates/service-desk/service-desk-login.php', ['errors' => $this->errors]);
             return;
         }
 
-        $user = ['id' => (int) $deskId, 'role' => 'service_desk'];
+        $user = ['id' => (int)$deskId, 'role' => 'service_desk'];
 
         Session::instance()->set('user', $user);
         Redirect::to('/dashboard');
