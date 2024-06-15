9068897800d0a60cf6685e14f0dd3a6b159d25eb
wuppo <wobjelsm@gmail.com>
2024-06-15 20:21:05 +0200

    fix: Enums

--- app/lib/classes/Enums/CRUDAction.php
+++ app/lib/classes/Enums/CRUDAction.php
@@ -8,14 +8,4 @@ enum CRUDAction
     case ACTION_READ;
     case ACTION_UPDATE;
     case ACTION_DELETE;
-
-//    public function getAction(): string
-//    {
-//        return match ($this) {
-//            self::ACTION_CREATE => 'create',
-//            self::ACTION_READ => 'read',
-//            self::ACTION_UPDATE => 'update',
-//            self::ACTION_DELETE => 'delete',
-//        };
-//    }
 }
