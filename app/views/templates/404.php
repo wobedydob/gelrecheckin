cc495ed8becefe6b4a2a8b225c980450205a6ed4
wuppo <wobjelsm@gmail.com>
2024-06-15 20:28:51 +0200

    fix: 404

--- app/views/templates/404.php
+++ app/views/templates/404.php
@@ -1,5 +1,5 @@
 <div class="not-found-container">
-    <h1>404 - NIET GEVONDEN</h1>
-    <p style="margin-bottom: 20px">De pagina die u probeert te openen is niet gevonden.</p>
-    <a href="<?php echo site_url(); ?>" class="button secondary">Terug naar Homepagina</a>
+    <h1 class="color-tertiary">404 - NIET GEVONDEN</h1>
+    <p class="color-tertiary mb-20">De pagina die u probeert te openen is niet gevonden.</p>
+    <a href="<?php echo site_url(); ?>" class="button tertiary">Terug naar Homepagina</a>
 </div>
