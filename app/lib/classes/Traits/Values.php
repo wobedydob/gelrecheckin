bb4c0e4386b71d304be9ef2a535c22461cb88454
wuppo <wobjelsm@gmail.com>
2024-06-15 20:12:32 +0200

    fix: Traits

--- app/lib/classes/Traits/Values.php
+++ app/lib/classes/Traits/Values.php
@@ -6,16 +6,35 @@ trait Values
 {
     protected array $values = [];
 
+    /**
+     * Magic method to retrieve the value of a dynamic property.
+     *
+     * @param string $name The name of the property to retrieve.
+     * @return mixed|null The value of the property if set, otherwise null.
+     */
     public function __get(string $name)
     {
         return $this->values[$name] ?? null;
     }
 
-    public function __set(string $name, $value): void
+    /**
+     * Magic method to set the value of a dynamic property.
+     *
+     * @param string $name The name of the property to set.
+     * @param mixed $value The value to assign to the property.
+     * @return void
+     */
+    public function __set(string $name, mixed $value): void
     {
         $this->values[$name] = $value;
     }
 
+    /**
+     * Magic method to check if a dynamic property is set.
+     *
+     * @param string $name The name of the property to check.
+     * @return bool True if the property is set, false otherwise.
+     */
     public function __isset(string $name): bool
     {
         return isset($this->values[$name]);
